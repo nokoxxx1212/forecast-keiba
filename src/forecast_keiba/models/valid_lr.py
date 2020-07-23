1e110b944d4fbbab42ded5dcc7123d944ebdd45b
noko_git <kotaroyamano0725@gmail.com>
2020-07-24 01:21:00 +0900

    update valid notebooks and scripts shuffle confiremed (#14)

--- src/forecast_keiba/models/valid_lr.py
+++ src/forecast_keiba/models/valid_lr.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 
 def valid_lr(race_results_df_processed_valid, model_lr):
     # 検証のデータ準備
@@ -15,33 +16,56 @@ def valid_lr(race_results_df_processed_valid, model_lr):
     valid_results_df = pd.DataFrame({'pred':y_valid_pred,'actual':y_valid})
     race_id_list = list(set(list(valid_results_df.index)))
     valid_results_list = valid_results_df.reset_index().values.tolist()
+    # シャッフル
+    random.shuffle(valid_results_list)
 
-    # 集計（1位正解率）
+    # 集計（馬単）
     correct_count = 0
     for race_id in race_id_list:
-        pred_1_cnt_by_race = 0
-        for i in range(len(valid_results_list)):
-            # 対象レースidのうち、一位と予測された馬
-            if valid_results_list[i][0] == race_id and valid_results_list[i][1] == 1:
-                pred_1_cnt_by_race += 1
-                # 対象レースidのうち一位と予測された馬が一つ目で、かつ結果も1位の場合
-                if pred_1_cnt_by_race == 1 and valid_results_list[i][2] == 1:
-                    correct_count += 1
-    print('rank1_acc: ' + str(correct_count/100))
+        pred_cnt_by_race = 0
+        cnt_by_race = 0
+        for rank in [1]:
+            for i in range(len(valid_results_list)):
+                # 対象レースidのうち、{rank}位と予測された馬
+                if valid_results_list[i][0] == race_id and valid_results_list[i][1] == rank:
+                    pred_cnt_by_race += 1
+                    if pred_cnt_by_race <= 1 and (valid_results_list[i][2] == 1):
+                        cnt_by_race += 1
+        if cnt_by_race == 1:
+            correct_count += 1
+    print('acc_exacta_1: ' + str(correct_count/100))
 
-    # 集計（1-3位正解率）
+    # 集計（馬連）
     correct_count = 0
     for race_id in race_id_list:
-        pred_3_cnt_by_race = 0
+        pred_cnt_by_race = 0
+        cnt_by_race = 0
+        for rank in [1, 2]:
+            for i in range(len(valid_results_list)):
+                # 対象レースidのうち、{rank}位と予測された馬
+                if valid_results_list[i][0] == race_id and valid_results_list[i][1] == rank:
+                    pred_cnt_by_race += 1
+                    if pred_cnt_by_race <= 2 and (valid_results_list[i][2] == 1 or valid_results_list[i][2] == 2):
+                        cnt_by_race += 1
+        if cnt_by_race == 2:
+            correct_count += 1
+    print('acc_quinella_2: ' + str(correct_count/100))
+
+    # 集計（三連複）
+    correct_count = 0
+    for race_id in race_id_list:
+        pred_cnt_by_race = 0
+        cnt_by_race = 0
         for rank in [1, 2, 3]:
             for i in range(len(valid_results_list)):
                 # 対象レースidのうち、{rank}位と予測された馬
                 if valid_results_list[i][0] == race_id and valid_results_list[i][1] == rank:
-                    pred_3_cnt_by_race += 1
-                    # 対象レースidのうち一位と予測された馬が一つ目で、かつ結果も1位の場合
-                    if pred_3_cnt_by_race <= 3 and valid_results_list[i][2] == 1 or valid_results_list[i][2] == 2 or valid_results_list[i][2] == 3:
-                        correct_count += 1
-    print('rank3_acc: ' + str(correct_count/300))
+                    pred_cnt_by_race += 1
+                    if pred_cnt_by_race <= 3 and (valid_results_list[i][2] == 1 or valid_results_list[i][2] == 2 or valid_results_list[i][2] == 3):
+                        cnt_by_race += 1
+        if cnt_by_race == 3:
+            correct_count += 1
+    print('acc_trio_3: ' + str(correct_count/100))
 
 
 def main(race_results_df_processed_valid, model_lr):
